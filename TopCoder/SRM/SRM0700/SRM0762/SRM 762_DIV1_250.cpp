f42ac013831415decc1ef443008f57e8a46f89d4
MitI_7 <MitI_7@outlook.com>
2020-03-09 18:18:31 +0900

    add srm

---
+++ TopCoder/SRM/SRM0700/SRM0762/SRM 762_DIV1_250.cpp
@@ -0,0 +1,279 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <climits>
+#include <assert.h>
+#include <iomanip>
+#include <bitset>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <functional>
+#include <fstream>
+#include <random>
+
+#define LEN(x) int(x.size())
+#define FOR(i,a,n) for(int i=(a);i<(n); ++i)
+#define FOE(i,a) for(auto i : a)
+#define ALL(c) (c).begin(), (c).end()
+#define RALL(c) (c).rbegin(), (c).rend()
+#define SUM(x) std::accumulate(ALL(x), 0LL)
+#define MIN(v) *std::min_element(v.begin(), v.end())
+#define MAX(v) *std::max_element(v.begin(), v.end())
+#define EXIST(v,x) (std::find(v.begin(), v.end(), x) != v.end())
+#define IS_BIT_ON(bit, i) (bit & (1 << i))
+#define BIT_ON(bit, i) (bit |= (1 << i))
+#define BIT_OFF(bit, i) (bit &= ~(1 << i))
+#define BIT_COUNT(bit) (__builtin_popcount(bit))
+
+typedef long long LL;
+template<typename T> std::vector<T> make_v(size_t a){return std::vector<T>(a);}
+template<typename T,typename... Ts> auto make_v(size_t a, Ts... ts){ return std::vector<decltype(make_v<T>(ts...))>(a,make_v<T>(ts...));}    // C++14
+template<typename T,typename V> typename std::enable_if<std::is_class<T>::value==0>::type fill_v(T &t,const V &v){t=v;}
+template<typename T,typename V> typename std::enable_if<std::is_class<T>::value!=0>::type fill_v(T &t,const V &v){for(auto &e:t) fill_v(e,v);}
+template<class T> inline T ceil(T a, T b) { return (a + b - 1) / b; }
+void print() { std::cout << std::endl; }
+template <class Head, class... Tail> void print(Head&& head, Tail&&... tail) { std::cout << head; if (sizeof...(tail) != 0) {std::cout << " ";} print(std::forward<Tail>(tail)...); }
+template <class T> void print(std::vector<T> &v) {for (auto& a : v) { std::cout << a; if (&a != &v.back()) {std::cout << " ";} }std::cout << std::endl;}
+template <class T> void print(std::vector<std::vector<T>> &vv) { for (auto& v : vv) { print(v); }}
+void debug() { std::cerr << std::endl; }
+template <class Head, class... Tail> void debug(Head&& head, Tail&&... tail) { std::cerr << head; if (sizeof...(tail) != 0) {std::cerr << " ";} print(std::forward<Tail>(tail)...); }
+template <class T> void debug(std::vector<T> &v) {for (auto& a : v) { std::cerr << a; if (&a != &v.back()) {std::cerr << " ";} }std::cerr << std::endl;}
+template <class T> void debug(std::vector<std::vector<T>> &vv) { for (auto& v : vv) { print(v); }}
+inline bool inside(long long y, long long x, long long H, long long W) {return 0 <= y and y < H and 0 <= x and x < W; }
+template<class T> inline double euclidean_distance(T y1, T x1, T y2, T x2) { return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)); }
+template<class T> inline double manhattan_distance(T y1, T x1, T y2, T x2) { return abs(x1 - x2) + abs(y1 - y2); }
+template<typename T> T &chmin(T &a, const T &b) { return a = std::min(a, b); }
+template<typename T> T &chmax(T &a, const T &b) { return a = std::max(a, b); }
+
+template<class T> inline std::vector<T> unique(std::vector<T> v) {
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    return v;
+}
+
+long long sum_of_arithmetic_progression(long long s, long long d, long long n) {
+    return n * (2 * s + (n - 1) * d) / 2;
+}
+
+bool is_power_of_two(long long x) {
+    return !(x & (x - 1));
+}
+
+long long gcd(long long a, long long b) {
+    if (b == 0) { return a; }
+    return gcd(b, a % b);
+}
+
+long long gcd(std::vector<long long> &v) {
+    long long ans = v[0];
+    for (int i = 1; i < (int)v.size(); ++i) {
+        ans = gcd(ans, v[i]);
+    }
+    return ans;
+}
+
+long long lcm(long long a, long long b) {
+    long long g = gcd(a, b);
+    return a / g * b;
+}
+
+const int INF = 1u << 30u;
+const long long LINF = 1ull << 60u;
+const double EPS = 1e-9;
+const long double PI = acos(-1.0);
+const std::vector<int> dy2 = {0, 1}, dx2 = {1, 0};
+const std::vector<int> dy4 = {0, 1, 0, -1}, dx4 = {1, 0, -1, 0};
+const std::vector<int> dy8 = { 0, -1, 0, 1, 1, -1, -1, 1 }, dx8 = { 1, 0, -1, 0, 1, 1, -1, -1 };
+
+using namespace std;
+
+struct LexicographicPartition {
+    int n;
+    vector<int> Aprefix;
+    int seed;
+    int Arange;
+    vector<int> positiveSum(int _n, vector<int> _Aprefix, int _seed, int _Arange) {
+        n = _n, Aprefix = _Aprefix, seed = _seed, Arange = _Arange;
+
+        auto A = make_v<LL>(n);
+        FOR(i, 0, LEN(Aprefix)) {
+            A[i] = Aprefix[i];
+        }
+
+        LL state = seed;
+        FOR(i, LEN(Aprefix), n) {
+            state = (1103515245LL * state + 12345);
+            A[i] = state % (2LL * Arange + 1);
+            A[i] = A[i] - Arange;
+            state = state % (1ULL << 31ULL);
+        }
+
+        LL total = 0;
+        FOR(i, 0, n) {
+            total += A[i];
+        }
+        if (total <= 0) {
+            return {-1};
+        }
+
+        auto B = make_v<int>(0);
+        LL now = 0;
+        LL length = 0;
+        FOR(i, 0, n) {
+            now += A[i];
+            length++;
+            total -= A[i];
+
+            if (now > 0 and (i == n - 1 or total > 0)) {
+                B.emplace_back(length);
+                length = 0;
+                now = 0;
+            }
+        }
+
+        if (length != 0) {
+            B.emplace_back(length);
+        }
+
+        auto ans = make_v<int>(0);
+        ans.emplace_back(LEN(B));
+        FOR(i, 0, min(200, LEN(B))) {
+            ans.emplace_back(B[i]);
+        }
+
+        return ans;
+    }
+};
+
+// CUT begin
+ifstream data("../SRM 762_DIV1_250.sample");
+
+string next_line() {
+    string s;
+    getline(::data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+template <typename T> string to_string(vector<T> ts) {
+    stringstream s;
+    s << "[ ";
+    for (int i = 0; i < ts.size(); ++i) {
+        if (i > 0) s << ", ";
+        s << to_string(ts[i]);
+    }
+    s << " ]";
+    return s.str();
+}
+
+bool do_test(int n, vector<int> Aprefix, int seed, int Arange, vector<int> __expected) {
+    time_t startClock = clock();
+    LexicographicPartition *instance = new LexicographicPartition();
+    vector<int> __result = instance->positiveSum(n, Aprefix, seed, Arange);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int n;
+        from_stream(n);
+        vector<int> Aprefix;
+        from_stream(Aprefix);
+        int seed;
+        from_stream(seed);
+        int Arange;
+        from_stream(Arange);
+        next_line();
+        vector<int> __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(n, Aprefix, seed, Arange, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1583405182;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "LexicographicPartition (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
