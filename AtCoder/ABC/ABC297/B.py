59440e6083437bb2e5da43f03540fb31f28d9cd1
MitI_7 <MitI_7@outlook.com>
2024-01-23 14:09:26 +0900

    add ABC297 B

---
+++ AtCoder/ABC/ABC297/B.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+from collections import defaultdict, Counter
+from itertools import product, groupby, count, permutations, combinations
+from math import pi, sqrt
+from collections import deque
+from bisect import bisect, bisect_left, bisect_right
+from string import ascii_lowercase
+from functools import lru_cache
+import sys
+
+sys.setrecursionlimit(500000)
+INF = float("inf")
+YES, Yes, yes, NO, No, no = "YES", "Yes", "yes", "NO", "No", "no"
+dy4, dx4 = [0, 1, 0, -1], [1, 0, -1, 0]  # 右，上，左，下
+dy8, dx8 = [0, -1, 0, 1, 1, -1, -1, 1], [1, 0, -1, 0, 1, 1, -1, -1]  # 右，下，左，上，右上，右下，左下，左上
+
+
+# memo
+# print(f"0埋めで10桁表示: {123:010}")
+# print(f"小数点以下2桁を表示: {123.456:.2f}")
+
+def is_bit_on(bit, i):
+    return (bit >> i) & 1
+
+
+def get_bit_set(bit, i, b):
+    assert (b == 0 or b == 1)
+    if b == 0:
+        return bit & ~(1 << i)
+    else:
+        return bit | (1 << i)
+
+
+def inside(y, x, H, W):
+    return 0 <= y < H and 0 <= x < W
+
+
+def ceil(a, b):
+    return (a + b - 1) // b
+
+
+def sum_of_arithmetic_progression(s, d, n):
+    return n * (2 * s + (n - 1) * d) // 2
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+def lcm(a, b):
+    g = gcd(a, b)
+    return a // g * b
+
+
+def solve():
+    S = input()
+    bs = []
+    rs = []
+    k = -1
+    for i in range(len(S)):
+        if S[i] == 'B':
+            bs.append(i)
+        if S[i] == 'R':
+            rs.append(i)
+        if S[i] == 'K':
+            k = i
+
+    if bs[0] % 2 == bs[1] % 2:
+        print("No")
+        return
+
+    if not (rs[0] < k < rs[1]):
+        print("No")
+        return
+
+    print("Yes")
+
+
+def main():
+    solve()
+
+
+if __name__ == '__main__':
+    main()
