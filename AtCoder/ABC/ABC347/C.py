a1dd1a54f57a313bb5852442f7a312e336528d68
MitI_7 <miti_7@outlook.com>
2025-02-24 14:52:33 +0900

    add ABC347

---
+++ AtCoder/ABC/ABC347/C.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import os
+import random
+import sys
+import time
+from bisect import bisect, bisect_left, bisect_right
+from collections import defaultdict, Counter, deque
+from functools import lru_cache
+from itertools import product, groupby, count, permutations, combinations
+from math import pi, sqrt
+from string import ascii_lowercase, ascii_uppercase
+
+sys.setrecursionlimit(500000)
+INF = float("inf")
+YES, Yes, yes, NO, No, no = "YES", "Yes", "yes", "NO", "No", "no"
+dy4, dx4 = [0, 1, 0, -1], [1, 0, -1, 0]  # 右，上，左，下
+dy8, dx8 = [0, -1, 0, 1, 1, -1, -1, 1], [1, 0, -1, 0, 1, 1, -1, -1]  # 右，下，左，上，右上，右下，左下，左上
+
+
+# memo
+# print(f"0埋めで10桁表示: {123:010}")
+# print(f"小数点以下2桁を表示: {123.456:.2f}")
+
+def is_bit_on(bit, i):
+    return (bit >> i) & 1
+
+
+def get_bit_set(bit, i, b):
+    assert (b == 0 or b == 1)
+    if b == 0:
+        return bit & ~(1 << i)
+    else:
+        return bit | (1 << i)
+
+
+def inside(y, x, H, W):
+    return 0 <= y < H and 0 <= x < W
+
+
+def ceil(a, b):
+    return (a + b - 1) // b
+
+
+def sum_of_arithmetic_progression(s, d, n):
+    return n * (2 * s + (n - 1) * d) // 2
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+def lcm(a, b):
+    g = gcd(a, b)
+    return a // g * b
+
+
+def main():
+    N, A, B = map(int, input().split())
+    D = list(map(int, input().split()))
+
+    E = [d % (A + B) for d in D]
+    for i in range(N):
+        E.append(E[i] + A + B)
+    E.sort()
+
+    for i in range(len(E) - 1):
+        if E[i + 1] - E[i] >= B + 1:
+            print(Yes)
+            return
+
+    print(No)
+
+
+if __name__ == '__main__':
+    main()
---
+++ AtCoder/ABC/ABC347/E.cpp
@@ -0,0 +1,186 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define LEN(x) (long long)(x.size())
+#define FOR(i, a, n) for (int i = (a); i < (n); ++i)
+#define FOE(i, a) for (auto i : a)
+#define ALL(c) (c).begin(), (c).end()
+#define RALL(c) (c).rbegin(), (c).rend()
+#define BIT_COUNT32(bit) (__builtin_popcount(bit))
+#define BIT_COUNT64(bit) (__builtin_popcountll(bit))
+
+template<typename T> using MinPriorityQueue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+template<typename T> using MaxPriorityQueue = std::priority_queue<T>;
+
+// @formatter:off
+typedef long long LL;
+typedef __int128_t LLL;
+template<typename T> std::vector<T> make_v(size_t a){return std::vector<T>(a);}
+template<typename T,typename... Ts> auto make_v(size_t a, Ts... ts){ return std::vector<decltype(make_v<T>(ts...))>(a,make_v<T>(ts...));}    // C++14
+template<typename T,typename V> typename std::enable_if<std::is_class<T>::value==0>::type fill_v(T &t,const V &v){t=v;}
+template<typename T,typename V> typename std::enable_if<std::is_class<T>::value!=0>::type fill_v(T &t,const V &v){for(auto &e:t) fill_v(e,v);}
+template<class T> inline T ceil(T a, T b) { return (a + b - 1) / b; }
+void print() { std::cout << std::endl; }
+template <class Head, class... Tail> void print(Head&& head, Tail&&... tail) { std::cout << head; if (sizeof...(tail) != 0) {std::cout << " ";} print(std::forward<Tail>(tail)...); }
+template <class T> void print(std::vector<T> &v) {for (auto& a : v) { std::cout << a; if (&a != &v.back()) {std::cout << " ";} }std::cout << std::endl;}
+template <class T> void print(std::pair<T, T> &p) { std::cout << p.first << " " << p.second << std::endl; }
+void debug() { std::cerr << std::endl; }
+template <class Head, class... Tail> void debug(Head&& head, Tail&&... tail) { std::cerr << head; if (sizeof...(tail) != 0) {std::cerr << " ";} debug(std::forward<Tail>(tail)...); }
+template <class T> void debug(std::vector<T> &v) {for (auto& a : v) { std::cerr << a; if (&a != &v.back()) {std::cerr << " ";} }std::cerr << std::endl;}
+template <class T> void debug(std::pair<T, T> &p) { std::cerr << p.first << " " << p.second << std::endl; }
+inline bool inside(long long y, long long x, long long H, long long W) {return 0 <= y and y < H and 0 <= x and x < W; }
+template<class T> inline double euclidean_distance(T y1, T x1, T y2, T x2) { return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)); }
+template<class T> inline T euclidean_distance2(T y1, T x1, T y2, T x2) { return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2); }
+template<class T> inline T manhattan_distance(T y1, T x1, T y2, T x2) { return abs(x1 - x2) + abs(y1 - y2); }
+template<typename T> T &chmin(T &a, const T &b) { return a = std::min(a, b); }
+template<typename T> T &chmax(T &a, const T &b) { return a = std::max(a, b); }
+bool is_bit_on(const unsigned long long bit, const unsigned int i) { return (bit >> i) & 1u; }
+unsigned long long get_bit_set(const unsigned long long bit, const unsigned int i, const unsigned int b) { assert(b == 0 or b == 1); if (b == 0) { return bit & ~(1ull << i); } else {return bit | (1ull << i);}}
+
+// 初項s交差d長さnの数列の和
+long long sum_of_arithmetic_progression(long long s, long long d, long long n) {
+    return n * (2 * s + (n - 1) * d) / 2;
+}
+
+// 三角数
+long long triangular_number(long long n) {
+    return n * (n + 1) / 2;
+}
+
+// sqrt(x)の整数解を求める
+// 整数解がなければ-1
+long long sqrt_integer(const long long x) {
+    if (x < 0) {
+        return -1;
+    }
+    auto a = (long long)sqrt(x);
+    if (a * a == x) {
+        return a;
+    }
+    if((a - 1) * (a - 1) == x) {
+        return a - 1;
+    }
+    if((a + 1) * (a + 1) == x) {
+        return a + 1;
+    }
+
+    return -1;
+}
+
+// xが2の階乗かどうか判定
+bool is_power_of_two(long long x) {
+    return !(x & (x - 1));
+}
+
+// O(log max(a, b))
+long long gcd(long long a, long long b) {
+    if (b == 0) { return a; }
+    return gcd(b, a % b);
+}
+
+long long lcm(long long a, long long b) {
+    long long g = gcd(a, b);
+    return a / g * b;
+}
+
+const int INF = 1u << 30u;  // 1,073,741,824
+const long long LINF = 1ull << 60u;
+const double EPS = 1e-9;
+const long double PI = acos(-1.0);
+// 2次元配列上での移動．右，下，左，上，右上，右下，左下，左上
+const std::vector<int> dy8 = {0, 1, 0, -1, -1, 1, 1, -1}, dx8 = {1, 0, -1, 0, 1, 1, -1, -1};
+// @formatter:on
+
+using namespace std;
+
+#include <cassert>
+#include <vector>
+
+template<typename T>
+class CumulativeSum {
+private:
+    std::vector<T> memo;
+
+public:
+    CumulativeSum() {
+    }
+
+    CumulativeSum(const std::vector<T> &line) {
+        this->build(line);
+    }
+
+    void build(const std::vector<T> &line) {
+        this->memo = std::vector<T>(line.size() + 1, 0);
+        for (int i = 0; i < int(line.size()); ++i) {
+            this->memo[i + 1] = this->memo[i] + line[i];
+        }
+    }
+
+    // sum[left, right)
+    T sum(const int left, const int right) const {
+        assert(right >= left);
+        return this->memo[right] - this->memo[left];
+    }
+};
+
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> X(Q);
+
+    FOR(i, 0, Q) {
+        cin >> X[i];
+        X[i]--;
+    }
+
+    vector<LL> time(Q);
+    unordered_set<int> s;
+    map<int, vector<int>> m;
+    FOR(i, 0, Q) {
+        if (s.contains(X[i])) {
+            s.erase(X[i]);
+        }
+        else {
+            s.insert(X[i]);
+        }
+        time[i] = LEN(s);
+        m[X[i]].emplace_back(i);
+    }
+    FOR(i, 0, 200010) {
+        m[i].emplace_back(Q);
+    }
+
+    CumulativeSum cs(time);
+
+    vector<LL> ans(N);
+    for (const auto &[k, v] : m) {
+        for (int i = 0; i < LEN(v); i += 2) {
+            if (i + 1 < LEN(v)) {
+                ans[k] += cs.sum(v[i], v[i + 1]);
+            }
+        }
+    }
+    print(ans);
+
+    return 0;
+}
