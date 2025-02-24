bbacf9d2d85ad743e25c90044cdcc7bdc68f8f72
MitI_7 <miti_7@outlook.com>
2025-02-24 14:54:05 +0900

    add ABC356

---
+++ AtCoder/ABC/ABC356/B.py
@@ -0,0 +1,58 @@
+import sys
+from bisect import bisect, bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from functools import lru_cache
+from itertools import combinations, count, groupby, permutations, product
+from math import gcd, lcm, pi, sqrt
+from string import ascii_lowercase, ascii_uppercase
+
+sys.setrecursionlimit(500000)
+INF = float("inf")
+YES, Yes, yes, NO, No, no = "YES", "Yes", "yes", "NO", "No", "no"
+dy4, dx4 = [0, 1, 0, -1], [1, 0, -1, 0]  # 右，上，左，下
+dy8, dx8 = [0, -1, 0, 1, 1, -1, -1, 1], [1, 0, -1, 0, 1, 1, -1, -1]  # 右，下，左，上，右上，右下，左下，左上
+
+
+# memo
+# print(f"0埋めで10桁表示: {123:010}")
+# print(f"小数点以下2桁を表示: {123.456:.2f}")
+
+def is_bit_on(bit, i):
+    return (bit >> i) & 1
+
+
+def get_bit_set(bit, i, b):
+    assert (b == 0 or b == 1)
+    return bit & ~(1 << i) if b == 0 else bit | (1 << i)
+
+
+def inside(y, x, H, W):
+    return 0 <= y < H and 0 <= x < W
+
+
+def ceil(a, b):
+    return (a + b - 1) // b
+
+
+def solve():
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    for i in range(N):
+        X = list(map(int, input().split()))
+        for j, x in enumerate(X):
+            A[j] -= x
+
+    for a in A:
+        if a > 0:
+            print(No)
+            return
+    print(Yes)
+
+
+def main():
+    solve()
+
+
+if __name__ == '__main__':
+    main()
---
+++ AtCoder/ABC/ABC356/C.py
@@ -0,0 +1,65 @@
+import sys
+from bisect import bisect, bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from functools import lru_cache
+from itertools import combinations, count, groupby, permutations, product
+from math import gcd, lcm, pi, sqrt
+from string import ascii_lowercase, ascii_uppercase
+
+sys.setrecursionlimit(500000)
+INF = float("inf")
+YES, Yes, yes, NO, No, no = "YES", "Yes", "yes", "NO", "No", "no"
+dy4, dx4 = [0, 1, 0, -1], [1, 0, -1, 0]  # 右，上，左，下
+dy8, dx8 = [0, -1, 0, 1, 1, -1, -1, 1], [1, 0, -1, 0, 1, 1, -1, -1]  # 右，下，左，上，右上，右下，左下，左上
+
+
+# memo
+# print(f"0埋めで10桁表示: {123:010}")
+# print(f"小数点以下2桁を表示: {123.456:.2f}")
+
+def is_bit_on(bit, i):
+    return (bit >> i) & 1
+
+
+def get_bit_set(bit, i, b):
+    assert (b == 0 or b == 1)
+    return bit & ~(1 << i) if b == 0 else bit | (1 << i)
+
+
+def inside(y, x, H, W):
+    return 0 <= y < H and 0 <= x < W
+
+
+def ceil(a, b):
+    return (a + b - 1) // b
+
+
+def solve():
+    N, M, K = map(int, input().split())
+
+    use = []
+    result = []
+    for i in range(M):
+        l = list(input().split())
+        use.append([int(l[j]) - 1 for j  in range(1, len(l) - 1)])
+        result.append(l[-1] == 'o')
+
+    ans = 0
+    for b in range(1 << N):
+        ok = True
+        for i in range(M):
+            num = 0
+            for key in use[i]:
+                num += is_bit_on(b, key)
+            ok &= (num >= K) == result[i]
+        ans += ok
+
+    print(ans)
+
+
+def main():
+    solve()
+
+
+if __name__ == '__main__':
+    main()
