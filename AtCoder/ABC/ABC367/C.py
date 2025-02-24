88cc297ddbaa6978f4f162b6ff1574720d1dfa26
MitI_7 <miti_7@outlook.com>
2025-02-24 14:55:00 +0900

    add ABC367

---
+++ AtCoder/ABC/ABC367/C.py
@@ -0,0 +1,60 @@
+import random
+import sys
+from bisect import bisect, bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from functools import lru_cache
+from itertools import combinations, count, groupby, permutations, product
+from math import gcd, lcm, pi, sqrt
+from string import ascii_lowercase, ascii_uppercase
+
+sys.setrecursionlimit(500000)
+INF = float("inf")
+YES, Yes, yes, NO, No, no = "YES", "Yes", "yes", "NO", "No", "no"
+dy4, dx4 = [0, 1, 0, -1], [1, 0, -1, 0]  # 右，上，左，下
+dy8, dx8 = [0, -1, 0, 1, 1, -1, -1, 1], [1, 0, -1, 0, 1, 1, -1, -1]  # 右，下，左，上，右上，右下，左下，左上
+
+
+# memo
+# print(f"0埋めで10桁表示: {123:010}")
+# print(f"小数点以下2桁を表示: {123.456:.2f}")
+
+def is_bit_on(bit, i):
+    return (bit >> i) & 1
+
+
+def get_bit_set(bit, i, b):
+    assert (b == 0 or b == 1)
+    return bit & ~(1 << i) if b == 0 else bit | (1 << i)
+
+
+def inside(y, x, H, W):
+    return 0 <= y < H and 0 <= x < W
+
+
+def ceil(a, b):
+    return (a + b - 1) // b
+
+
+def dfs(i, r, now, N, K, R):
+    if i >= N:
+        if r % K == 0:
+            print(*now)
+        return
+
+    for a in range(1, R[i] + 1):
+        dfs(i + 1, (r + a) % K, now + [a], N, K, R)
+
+
+def solve():
+    N, K = map(int, input().split())
+    R = list(map(int, input().split()))
+
+    dfs(0, 0, [], N, K, R)
+
+
+def main():
+    solve()
+
+
+if __name__ == '__main__':
+    main()
---
+++ AtCoder/ABC/ABC367/D.cpp
@@ -0,0 +1,97 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#define LEN(x) (long long)(x.size())
+#define FOR(i, a, n) for (int i = (a); i < (n); ++i)
+#define FOE(i, a) for (auto i : a)
+#define ALL(c) (c).begin(), (c).end()
+#define RALL(c) (c).rbegin(), (c).rend()
+#define BIT_COUNT32(bit) (__builtin_popcount(bit))
+#define BIT_COUNT64(bit) (__builtin_popcountll(bit))
+
+template<typename T> using MinPriorityQueue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+template<typename T> using MaxPriorityQueue = std::priority_queue<T>;
+
+// @formatter:off
+typedef long long LL;
+typedef __int128_t LLL;
+template<typename T> std::vector<T> make_v(size_t a){return std::vector<T>(a);}
+template<typename T,typename... Ts> auto make_v(size_t a, Ts... ts){ return std::vector<decltype(make_v<T>(ts...))>(a,make_v<T>(ts...));}    // C++14
+template<typename T,typename V> typename std::enable_if<std::is_class<T>::value==0>::type fill_v(T &t,const V &v){t=v;}
+template<typename T,typename V> typename std::enable_if<std::is_class<T>::value!=0>::type fill_v(T &t,const V &v){for(auto &e:t) fill_v(e,v);}
+template<class T> inline T ceil(T a, T b) { return (a + b - 1) / b; }
+void print() { std::cout << std::endl; }
+template <class Head, class... Tail> void print(Head&& head, Tail&&... tail) { std::cout << head; if (sizeof...(tail) != 0) {std::cout << " ";} print(std::forward<Tail>(tail)...); }
+template <class T> void print(std::vector<T> &v) {for (auto& a : v) { std::cout << a; if (&a != &v.back()) {std::cout << " ";} }std::cout << std::endl;}
+template <class T> void print(std::pair<T, T> &p) { std::cout << p.first << " " << p.second << std::endl; }
+void debug() { std::cerr << std::endl; }
+template <class Head, class... Tail> void debug(Head&& head, Tail&&... tail) { std::cerr << head; if (sizeof...(tail) != 0) {std::cerr << " ";} debug(std::forward<Tail>(tail)...); }
+template <class T> void debug(std::vector<T> &v) {for (auto& a : v) { std::cerr << a; if (&a != &v.back()) {std::cerr << " ";} }std::cerr << std::endl;}
+template <class T> void debug(std::pair<T, T> &p) { std::cerr << p.first << " " << p.second << std::endl; }
+inline bool inside(long long y, long long x, long long H, long long W) {return 0 <= y and y < H and 0 <= x and x < W; }
+template<class T> inline double euclidean_distance(T y1, T x1, T y2, T x2) { return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)); }
+template<class T> inline T euclidean_distance2(T y1, T x1, T y2, T x2) { return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2); }
+template<class T> inline T manhattan_distance(T y1, T x1, T y2, T x2) { return abs(x1 - x2) + abs(y1 - y2); }
+template<typename T> T &chmin(T &a, const T &b) { return a = std::min(a, b); }
+template<typename T> T &chmax(T &a, const T &b) { return a = std::max(a, b); }
+bool is_bit_on(const unsigned long long bit, const unsigned int i) { return (bit >> i) & 1u; }
+unsigned long long get_bit_set(const unsigned long long bit, const unsigned int i, const unsigned int b) { assert(b == 0 or b == 1); if (b == 0) { return bit & ~(1ull << i); } else {return bit | (1ull << i);}}
+
+const int INF = 1u << 30u;  // 1,073,741,824
+const long long LINF = 1ull << 60u;
+const double EPS = 1e-9;
+const long double PI = acos(-1.0);
+// 2次元配列上での移動．右，下，左，上，右上，右下，左下，左上
+const std::vector<int> dy8 = {0, 1, 0, -1, -1, 1, 1, -1}, dx8 = {1, 0, -1, 0, 1, 1, -1, -1};
+// @formatter:on
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int N, M;
+    cin >> N >> M;
+    vector<LL> A(N);
+
+    FOR(i, 0, N) {
+        cin >> A[i];
+    }
+
+    LL total = 0;
+    FOE(a, A) {
+        (total += a) %= M;
+    }
+
+    LL ans = 0;
+    LL dist = 0;
+    map<LL, LL> m;
+    m[0] = 1;
+    FOR(r, 0, N - 1) {
+        (dist += A[r]) %= M;
+        ans += m[dist];
+        ans += m[(dist - total + M) % M];
+        m[dist]++;
+    }
+    print(ans);
+
+    return 0;
+}
